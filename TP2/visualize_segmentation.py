67b70ed6d7daa4e321ec36ad8ae5c708c4dcebc6
samy <samy.aimeur@telecom-paris.fr>
2024-03-30 01:27:20 +0100

    trying to solve perl scrpit problem

--- TP2/visualize_segmentation.py
+++ TP2/visualize_segmentation.py
@@ -1,10 +1,11 @@
 def visualize_segmentation(mail_filename, visualized_mail_filename, path):
 	## @parameter mail_filename : Path to the mail on wich we try the algorithm.
-	## @parameter visualized_mail_filename : The path on which we write the mail with the v 	## @parameter path : The sequence of 0 and 1 that the Viterbi algorithm returns.
+	## @parameter visualized_mail_filename : The path on which we write the mail with the v 	
+	## @parameter path : The sequence of 0 and 1 that the Viterbi algorithm returns.
     ## return: True if the header corresponds to the
 	i=0
 	while path[i] == 0:
-	i+=1
+		i+=1
 	visu = open(visualized_mail_filename, 'w') 
 	if(mail_filename.endswith(".dat")):
 		mail_filename = mail_filename[:-4] + ".txt" 
@@ -16,7 +17,3 @@ def visualize_segmentation(mail_filename, visualized_mail_filename, path):
 	visu.close() 
 	mail.close() 
 	return
-
-visualize_segmentation("dat/mail1.dat", 'path_test.txt', path)
-
-Ceci est un test
